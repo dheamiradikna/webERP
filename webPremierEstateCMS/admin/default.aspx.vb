4d68ad518fc9a6de25f16d759885a23b54109102
Dhea Miradikna <44384934+dheamiradikna@users.noreply.github.com>
2025-05-10 01:28:24 +0700

    Add files via upload

---
+++ webPremierEstateCMS/admin/default.aspx.vb
@@ -0,0 +1,11 @@
+﻿Imports [class].clsWebGeneral
+
+Partial Class admin_default
+    Inherits System.Web.UI.Page
+
+    Protected rootPath As String = _rootPath
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Response.Redirect(rootPath + "wf/admin/default.aspx")
+    End Sub
+End Class
